cd9568c3230ecfcd39673f5a0c8180166b9f6712
KainAgava <143312166+KainAgava@users.noreply.github.com>
2023-09-28 14:33:45 +0700

    Add files via upload

---
+++ homeWork2.1/homeWork2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            Console.WriteLine("Что?");
+            text = Console.ReadLine();
+
+            int quantity;
+            Console.WriteLine("Сколько?");
+            quantity = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < quantity; i++)
+            {
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
